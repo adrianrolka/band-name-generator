49e0ceb8a02e15434b8b26a1c90fe83116bcbaaf
XerosPL <>
2022-04-21 19:59:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+print("What's name of the city you grew up in?")
+x = input()
+print('What\'s your pet\'s name?')
+y = input()
+print("Your band name could be", x, y)
